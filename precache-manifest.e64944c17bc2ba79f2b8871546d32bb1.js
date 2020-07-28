f0f77aad835fcaaba72bc878808cb6127d1124e0
KRVPerera <rukshan.viduranga@gmail.com>
2020-07-28 21:26:04 +0530

    Updates

---
+++ precache-manifest.e64944c17bc2ba79f2b8871546d32bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee2c3d4947ba3a3280af0b22d770063",
+    "url": "/OhMyData/index.html"
+  },
+  {
+    "revision": "f17394aa2e36e585894c",
+    "url": "/OhMyData/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e572afa5f9c68e24233a",
+    "url": "/OhMyData/static/js/2.818f3412.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OhMyData/static/js/2.818f3412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17394aa2e36e585894c",
+    "url": "/OhMyData/static/js/main.3f30689e.chunk.js"
+  },
+  {
+    "revision": "50ebb8bfeb521958708f",
+    "url": "/OhMyData/static/js/runtime-main.1976dd63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/OhMyData/static/media/logo.5d5d9eef.svg"
+  }
+]);
